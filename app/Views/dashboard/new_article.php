dbfdff2a5f790b1b358e255363b460dc62019526
Paul ZOBLATIN <zpaulange@gmail.com>
2024-05-07 08:20:12 +0100

    fix error

--- app/Views/dashboard/new_article.php
+++ app/Views/dashboard/new_article.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New article</title>
+    <link rel="stylesheet" href="<?=base_url('styles/css/icons.css')?>">
     <link rel="stylesheet" href="<?=base_url('styles/css/admin/dashboard.css')?>">
 </head>
 <body>
@@ -11,44 +12,68 @@
         <?=view('sections/sidebar')?>
 
         <div class="dashboard__right">
-            <h1 class="content__title">Nouveau article</h1>
-            <?=view('sections/error')?>
-            <form class="article-form" method="post" enctype="multipart/form-data" action="<?=base_url('articles/store')?>">
-                <div class="form-group">
-                    <label for="title">Titre de l'article</label>
-                    <input type="text" id="title" name="title" required>
+            <nav class="dashboard__nav">
+                <a href="#"><i class="icon icon-menu"></i></a>
+                <div class="dashboard__nav__profil">
+                    <a href="#" class="dashboard__nav__profilBtn" id="profilBtn">
+                        <span><i class="icon icon-user"></i></span>
+                    </a>
+                    <ul class="dashboard__nav__dropdown" id="dropdownProfil">
+                        <li>
+                            <a href="<?=base_url('/auth/logout')?>">
+                                <i class="icon icon-logout"></i>
+                                <span>Déconnexion</span>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-                <div class="form-group">
-                    <label for="description">Description de l'article</label>
-                    <textarea id="description" name="description" required></textarea>
+            </nav>
+            <div class="dashboard__main">
+                <div class="dashboard__main__title">
+                    <i class="icon icon-plus"></i>
+                    <p>Nouveau article</p>
                 </div>
-                <div class="form-group">
-                    <label for="publication-date">Date de publication</label>
-                    <input type="date" id="publication-date" name="date_pub" required>
-                </div>
-                <div class="form-group">
-                    <label for="image">Image</label>
-                    <input type="file" id="image" name="img">
-                </div>
-                <div class="form-group">
-                    <label for="category">Catégorie</label>
-                    <select id="category" name="category" required>
-                        <option value="">Sélectionnez une catégorie</option>
-                        <?php foreach($categories as $category): ?>
-                        <option value="<?=$category['name']?>"><?=$category['name']?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <div id="links-container">
-                        <!-- Les champs d'entrée pour les liens seront ajoutés ici -->
-                    </div>
-                    <button type="button" id="add-link">Ajouter un lien</button>
-                    <button type="button" id="remove-link">Supprimer un lien</button>
 
+                <div class="dashboard__main__box">
+                
+                    <?=view('sections/error')?>
+                    <form class="article-form" method="post" enctype="multipart/form-data" action="<?=base_url('articles/store')?>">
+                        <div class="form-group">
+                            <label for="title">Titre de l'article</label>
+                            <input type="text" id="title" name="title" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description de l'article</label>
+                            <textarea id="description" name="description" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="publication-date">Date de publication</label>
+                            <input type="date" id="publication-date" name="date_pub" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="image">Image</label>
+                            <input type="file" id="image" name="img">
+                        </div>
+                        <div class="form-group">
+                            <label for="category">Catégorie</label>
+                            <select id="category" name="category" required>
+                                <option value="">Sélectionnez une catégorie</option>
+                                <?php foreach($categories as $category): ?>
+                                <option value="<?=$category['name']?>"><?=$category['name']?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <div id="links-container">
+                                <!-- Les champs d'entrée pour les liens seront ajoutés ici -->
+                            </div>
+                            <button type="button" id="add-link">Ajouter un lien</button>
+                            <button type="button" id="remove-link">Supprimer un lien</button>
+
+                        </div>
+                        <button type="submit" class="submit-button">Créer l'article</button>
+                    </form>
                 </div>
-                <button type="submit" class="submit-button">Créer l'article</button>
-            </form>
             <!-- Le reste du contenu reste inchangé -->
         </div>
     </div>
